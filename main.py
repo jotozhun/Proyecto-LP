2327e72df0ccdafa141af6e22d2511c9727214f5
Joel Torres <jotozhun@espol.edu.ec>
2020-11-14 15:11:33 -0500

    Tokens añadidos y algunos ejemplos en archivo.txt

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 import ply.lex as lex
 
+tokens = [
+    'STRINGVAL',
+    'NUMBER',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'COMMA',
+    'SEMICOLON',
+    'LPAREN',
+    'RPAREN',
+    'LBRACE',
+    'RBRACE',
+    'LESSTHAN',
+    'MORETHAN',
+    'ASSIGN',
+    'ID',
+]
+
 reserved = {
     'num':'NUM',
     'int':'INT',
     'double':'DOUBLE',
     'bool': 'BOOL',
+    'true':'TRUE',
+    'false':'FALSE',
     'var':'VAR',
-    'string':'STRING',
+    'String':'STRING',
     'dynamic': 'DYNAMIC',
     'if':'IF',
     'else':'ELSE',
@@ -17,16 +38,7 @@ reserved = {
     'Set':'SET'
 }
 
-tokens = [
-    'NUMBER',
-    'PLUS',
-    'MINUS',
-    'TIMES',
-    'DIVIDE',
-    'LPAREN',
-    'RPAREN',
-    'ID',
-] + list(reserved.values())
+tokens += list(reserved.values())
 
 t_PLUS = r'\+'
 t_MINUS = r'-'
@@ -34,6 +46,14 @@ t_TIMES = r'\*'
 t_DIVIDE = r'/'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
+#t_STRINGVAL = r'(^\'[^\']*\'$|^\"[^\']*\"$)'
+t_ASSIGN = r'='
+t_LBRACE = r'{'
+t_RBRACE = r'}'
+t_LESSTHAN = r'<'
+t_MORETHAN = r'>'
+t_COMMA = r','
+t_SEMICOLON = r';'
 
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z0-9_]*'
@@ -41,8 +61,13 @@ def t_ID(t):
     return t
 
 def t_NUMBER(t):
-    r'\d+'
-    t.value = int(t.value)
+    #r'\d+'
+    r'[+-]?\d + (\.\d+)?'
+    return t
+
+def t_STRINGVAL(t):
+    r'"{1}([^\\"]|\\(.|\n))*"{1}'
+    t.lexer.lineno += t.value.count("\n")
     return t
 
 def t_newline(t):
@@ -61,12 +86,12 @@ def t_error(t):
 
 lexer = lex.lex()
 
-data = """
-MAP<int> for efe
-"""
-lexer.input(data)
-while True:
-    tok = lexer.token()
-    if not tok:
-        break
-    print(tok)
+file = open("archivo.txt")
+
+for line in file:
+    lexer.input(line)
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break  # No more input
+        print(tok)
