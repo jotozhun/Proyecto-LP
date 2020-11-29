d58db84b359db39bd3a239ee66ae0b61821abbf6
Joel Torres <jotozhun@espol.edu.ec>
2020-11-29 17:07:40 -0500

    Añadida plantilla del GUI

---
+++ GUI.py
@@ -0,0 +1,62 @@
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.resize(600, 600)
+        self.setWindowTitle("Lexic&Syntax")
+        self.textbox = QTextEdit(self)
+        self.resultText = QTextEdit(self)
+        self.init_UI()
+
+    def init_UI(self):
+        # La barra de herramientas
+        toolbar = QToolBar("My toolBar")
+        toolbar.setIconSize(QSize(32, 32))
+        self.addToolBar(toolbar)
+        self.setToolButtonStyle(Qt.ToolButtonIconOnly)
+
+        # Crear Iconos y botones
+
+        newButton = QAction(QIcon("Assets/new.png"), "Nuevo Archivo", self)
+        newButton.setStatusTip("Nuevo Archivo")
+        toolbar.addAction(newButton)
+
+        saveButton = QAction(QIcon("Assets/save.png"), "Guardar Archivo", self)
+        saveButton.setStatusTip("Guardar Archivo")
+        toolbar.addAction(saveButton)
+
+        lexicoButton = QAction(QIcon("Assets/lex.png"), "Analizar léxico", self)
+        lexicoButton.setStatusTip("Ejecutar analizador léxico")
+        toolbar.addAction(lexicoButton)
+
+        syntaxButton = QAction(QIcon("Assets/synt.png"), "Analizar sintáctica", self)
+        syntaxButton.setStatusTip("Ejecutar analizador sintáctico")
+        toolbar.addAction(syntaxButton)
+
+        # Cuadro de texto
+
+        self.textbox.move(5, 45)
+        self.textbox.resize(345, 550)
+        self.textbox.setCurrentFont(QFont("Times", 18))
+
+        self.resultText.move(355, 45)
+        self.resultText.resize(240, 550)
+        self.resultText.setReadOnly(True)
+
+        # Cuadro de resultados
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec_()
+
+print("Termina el event loop")
